9eaeaac3250f84543c1ba2036b1a7f935a4958aa
Mgo <mgotm13@gmail.com>
2020-07-30 00:00:10 +0400

    Header and Navigation

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,27 @@
 <template>
   <div>
+    <Header @sidenavToggle="displaySideNav = !displaySideNav "/>
+    <SideNav
+      :show="displaySideNav"
+      @close="displaySideNav=false"
+    />
     <Nuxt/>
   </div>
 </template>
 
 <script lang="ts">
   import {Component, Vue} from "nuxt-property-decorator";
+  import Header from "~/components/Navigation/Header";
+  import SideNav from "~/components/Navigation/Sidenav";
 
   @Component({
     components: {
+      Header,
+      SideNav
     }
   })
   export default class Default extends Vue {
-
+    private displaySideNav: boolean = false;
   }
 </script>
 
