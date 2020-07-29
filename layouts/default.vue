ca4280504a6855623ed8fe2e3d9d9d89d19bb4f1
Mgo <mgotm13@gmail.com>
2020-07-29 23:16:52 +0400

    styling and Components

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,17 +1,14 @@
 <template>
   <div>
-    <Header/>
     <Nuxt/>
   </div>
 </template>
 
 <script lang="ts">
   import {Component, Vue} from "nuxt-property-decorator";
-  import Header from "~/components/Header.vue";
 
   @Component({
     components: {
-      Header: Header
     }
   })
   export default class Default extends Vue {
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -14,10 +14,56 @@
   }
 </script>
 
-<style lang="">
+<style>
   .posts-page {
     display: flex;
     justify-content: center;
-    align-center: center;
+    align-items: center;
+  }
+
+  .post {
+    width: 100%;
+  }
+
+  @media (min-width: 768px) {
+    .post {
+      width: 600px;
+      margin: auto;
+    }
+  }
+
+  .post-title {
+    margin: 0;
+  }
+
+  .post-details {
+    padding: 10px;
+    box-sizing: border-box;
+    border-bottom: 3px solid #ccc;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+
+  @media (min-width: 768px) {
+    .post-details {
+      flex-direction: row;
+    }
+  }
+
+  .post-detail {
+    color: rgb(88, 88, 88);
+    margin: 0 10px;
+  }
+
+  .post-feedback a {
+    color: red;
+    text-decoration: none;
+  }
+
+  .post-feedback a:hover,
+  .post-feedback a:active {
+    color: salmon;
   }
 </style>
